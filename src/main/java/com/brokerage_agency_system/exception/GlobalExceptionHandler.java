77230f816112c8ce355a5ab5892e171ed7bd174d
pl <gs3643739@abv.bg>
2024-10-21 12:01:30 +0300

    Angular initial commit

--- src/main/java/com/brokerage_agency_system/exception/GlobalExceptionHandler.java
+++ src/main/java/com/brokerage_agency_system/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.brokerage_agency_system.exception;
 import com.brokerage_agency_system.DTO.ApiResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,6 +13,23 @@ import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+
+        // List to store error messages
+        List<String> errorMessages = new ArrayList<>();
+
+        // Extracting field validation error messages
+        for (FieldError error : exception.getBindingResult().getFieldErrors()) {
+            errorMessages.add(error.getDefaultMessage());
+        }
+
+        // Return the list of error messages with a 400 BAD REQUEST status
+        return ResponseEntity
+                .badRequest()
+                .body(errorMessages);
+    }
 
     @ExceptionHandler(value = UserAlreadyExistsException.class)
     public ResponseEntity<ApiResponseDTO<?>> UserAlreadyExistsExceptionHandler(UserAlreadyExistsException exception) {
