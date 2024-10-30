412a7a7a0f312aa5ddeb19f3d53e68636c6604e0
pl <gs@abv.bg>
2024-10-30 23:56:42 +0200

    Fix Enum deserialization issues

--- src/main/java/com/brokerage_agency_system/model/EstateStatus.java
+++ src/main/java/com/brokerage_agency_system/model/EstateStatus.java
@@ -1,5 +1,29 @@
 package com.brokerage_agency_system.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum EstateStatus {
-    FREE, UNAVAILABLE, DOWN_PAID
+    FREE, UNAVAILABLE, DOWN_PAID;
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
+
+    @JsonCreator
+    public static EstateStatus fromValue(String value) {
+        try {
+            return EstateStatus.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            // Generate a parameterized exception message
+            String acceptedValues = Arrays.stream(EstateStatus.values())
+                    .map(EstateStatus::toValue)
+                    .collect(Collectors.joining(", "));
+            throw new IllegalArgumentException("Invalid EstateStatus value: " + value + ". Accepted values are: " + acceptedValues);
+        }
+    }
 }
